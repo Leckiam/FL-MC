c3afae5cfc9437ea8d5d669cdda92fbdabda7ba3
Maikel Andres Cisternas Mercado <mai.cisternas@duocuc.cl>
2023-09-01 16:06:38 -0400

    Avance del 01/09
    -Añadir la page "login" y la carpeta que almacenara pages (se llama "pages " la carpeta).
    -Añadir la page "message".
    -Añadir "tabs" en el content del .html de la page "home".
    -Hacer una vista básica de inicio de sesión en "login".
    -Hacer una vista básica de enviar mensaje en "message".
    -Crear el método "ingresar()", para realizar comunicacion entre paginas.
    -Comunicarse entre las pages de "login" y "home" a través de un button respectivo para cada page (usando el "(click)=ingresar()").

--- PetsFound/src/app/app-routing.module.ts
+++ PetsFound/src/app/app-routing.module.ts
@@ -4,12 +4,20 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'message',
+    loadChildren: () => import('./pages/message/message.module').then( m => m.MessagePageModule)
   },
 ];
 
