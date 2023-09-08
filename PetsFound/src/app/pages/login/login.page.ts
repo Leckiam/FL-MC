b8417d7d3405d0381dfb9cb9313c626706052b19
Maikel Andres Cisternas Mercado <mai.cisternas@duocuc.cl>
2023-09-08 15:18:52 -0300

    Ponerse al día con el main.
    -Agregar page "Register".
    -Agregar validaciones.
    -Actualización del README.

--- PetsFound/src/app/pages/login/login.page.ts
+++ PetsFound/src/app/pages/login/login.page.ts
@@ -23,7 +23,22 @@ export class LoginPage implements OnInit {
   ngOnInit() {
   }
 
+  validarLogin(user:any){
+    if (user.usuario.length >=6 && user.password.length >=6) {
+      return true
+    } else {
+      return false
+    } 
+  } 
+  changePage(namePage:any){
+    this.appComponentt.ingresar(namePage);
+  }
   changePageLog(namePage:any){
+  if (!this.validarLogin(this.user)){
+    let msg= 'Su usuario y/o contraseña no está dentro del rango de caracteres (6 caracteres)'
+    this.appComponentt.presentToast('bottom',msg)
+    
+  } else {
     let navegationExtras: NavigationExtras = {
       state:{
         user: this.user
@@ -31,7 +46,8 @@ export class LoginPage implements OnInit {
     }
     
     this.appComponentt.ingresar(namePage,navegationExtras);
-  }
+  }} 
+    
   ionViewWillEnter() {
     if (!this.appComponentt.returnFirstTime()) {
       window.location.reload();
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # FL-MC
+
+Repositorios para Compartir con nuestros fans :D.
+
+Los proyectos se suben sin su carpeta node_modules
+
+Para reconstruir la carpeta node_modules colocar en la terminal:
+
+npm install
+
+Para instalar Angular colocar en la terminar:
+- npm install -g @angular/cli
+  
+- ng add @angular/material
