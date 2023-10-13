99f0041cb85ee02061bdad129528ec8b604ed841
ElColorin <franciscolazcano178@gmail.com>
2023-10-13 18:43:57 -0300

    Avanzando :D
    Corrigiendo componente de agregar mascota

--- PetsFound/src/app/pages/home/home-routing.module.ts
+++ PetsFound/src/app/pages/home/home-routing.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePage } from './home.page';
+import { AgregarMascotaComponent } from 'src/app/components/agregar-mascota/agregar-mascota.component';
+
 
 const routes: Routes = [
   {
     path: '',
     component: HomePage,
-  }
+    children:[
+    {
+      path: 'agregar-mascota',
+      component: AgregarMascotaComponent,
+    },
+  
+   ]
+ }
 ];
-
 
 
 @NgModule({
--- PetsFound/src/app/pages/home/home.module.ts
+++ PetsFound/src/app/pages/home/home.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
+import { AgregarMascotaComponent } from 'src/app/components/agregar-mascota/agregar-mascota.component';
 
 import { HomePageRoutingModule } from './home-routing.module';
 
@@ -14,6 +15,6 @@ import { HomePageRoutingModule } from './home-routing.module';
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage, AgregarMascotaComponent]
 })
 export class HomePageModule {}
